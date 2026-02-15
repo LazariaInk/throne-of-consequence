e0f189bac23cd4ad61d3f4b576fc3041519680ee
LazariaInk <lorienlored@gmail.com>
2026-02-15 23:08:13 +0300

    spring init

---
+++ src/main/java/com/lazari/throne_of_consequence/ThroneOfConsequenceApplication.java
@@ -0,0 +1,14 @@
+package com.lazari.throne_of_consequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThroneOfConsequenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThroneOfConsequenceApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/lazari/throne_of_consequence/ThroneOfConsequenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazari.throne_of_consequence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThroneOfConsequenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
